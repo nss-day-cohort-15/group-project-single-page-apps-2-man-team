d59e2d508ec2ba583fde176fd26737396399e300
Matt Kraatz <matt.kraatz@gmail.com>
2016-07-27 14:30:27 -0500

    completed a few other minor DOM functions

--- messages.js
+++ messages.js
@@ -1,11 +1,20 @@
 var Chatty = (function(chatty){
   var output = document.querySelector(".chat-window")
+  // Print a provided argument to the bottom of the messages window with a delete button
   chatty.print = function(text) {
     output.innerHTML += `
       <div>
-        ${text} <button class="delete">Delete<button>
+        ${text} <button type="button" class="btn btn-xs btn-danger">Delete</button>
       </div>
       `
   }
+  // Function to clear the messages window
+  chatty.clear = function() {
+    output.innerHTML = ""
+  }
+  clear.addEventListener("click",chatty.clear)
+  // Function to clear a specific line as specified by the delete button
+
+  document.addEventListener("click",chatty.eventCheck)
   return chatty
 })(Chatty || {})
--- xml.js
+++ xml.js
@@ -1,16 +1,19 @@
 var Chatty = (function(chatty) {
   var data = {}
+  // Load JSON data on load and print to messages window
   chatty.loadData = (function() {
     var xml = new XMLHttpRequest();
     xml.addEventListener("load",function(){
       data = JSON.parse(xml.responseText)
-      forEach (i in data) {
+      console.dir(data)
+      for (i in data) {
         chatty.print(data[i])
       }
     })
     xml.open("GET","messages.json")
     xml.send()
   })()
+  // Function to return JSON data
   chatty.getData = function(){
     return data;
   }
