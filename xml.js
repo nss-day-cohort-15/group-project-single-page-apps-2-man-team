0444e57f25ab3b9fd62fe19ff73545927fba5962
Matt Kraatz <matt.kraatz@gmail.com>
2016-07-27 13:53:22 -0500

    completed boilerplate XMLHttpRequest

--- xml.js
+++ xml.js
@@ -1,6 +1,15 @@
-var xml = new XMLHttpRequest();
-xml.addEventListener("load",function(){
-  console.dir(xml)
-})
-xml.open("GET","messages.json")
-xml.send()
+var Chatty = (function(chatty) {
+  var data = {}
+  chatty.loadData = (function() {
+    var xml = new XMLHttpRequest();
+    xml.addEventListener("load",function(){
+      data = JSON.parse(xml.responseText)
+    })
+    xml.open("GET","messages.json")
+    xml.send()
+  })()
+  chatty.getData = function(){
+    return data;
+  }
+  return chatty
+})(Chatty || {})
